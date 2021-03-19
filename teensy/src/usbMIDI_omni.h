fa5c5d40b8a27b88a8adfc8383f8b61a86c2d2e3
Jacob Parmer <jacob.parmer@auburn.edu>
2021-03-19 15:34:36 -0500

    Added some MIDI functionality

--- teensy/src/usbMIDI_omni.h
+++ teensy/src/usbMIDI_omni.h
@@ -1,26 +1,31 @@
 // Author: Jacob Parmer
 // Date: Mar 8, 2021
+#include <vector>
+#include <array>
 
-#define NOTE_OFF 0x80
-#define NOTE_ON 0x90
-#define AFTERTOUCH 0xA0
-#define CONTIN_CONTROL 0xB0
-#define PATCH_CHANGE 0xC0
-#define CHANNEL_PRESSURE 0xD0
-#define PITCH_BEND 0xE0
+#define NOTE_OFF 0x64
+#define NOTE_ON 0x61
+#define AFTERTOUCH 0xA1
+#define CONTIN_CONTROL 0xB1
+#define PATCH_CHANGE 0xC1
+#define CHANNEL_PRESSURE 0xD1
+#define PITCH_BEND 0xE1
 
 #define MIDI_PACKET_SIZE 3
+
+typedef std::array<byte, MIDI_PACKET_SIZE> midi_message;
 
 class OmniMIDI {
 
     public:
         OmniMIDI();
         void read();
-        void omniNoteOn();
-        void omniNoteOff();
+        void omniNoteOn(midi_message msg);
+        void omniNoteOff(midi_message msg);
 
     private:
-        byte last_received_packet[MIDI_PACKET_SIZE];
+        midi_message last_received_message;
+        std::vector<midi_message> notes_on;
 
 };
 
@@ -30,40 +35,39 @@ OmniMIDI::OmniMIDI() {
 
 void OmniMIDI::read() {
 
-    while (Serial.available() != 0) {
+    if (Serial.available() != 0) {
 
-        if ((Serial.available() % MIDI_PACKET_SIZE) == 0) {
-
+        while ((Serial.available() % MIDI_PACKET_SIZE) != 0) {
+            Serial.print("Waiting on rest of midi packet. Size of serial queue: ");
             Serial.println(Serial.available());
+            delay(200);
+        }; // waits for full MIDI packet to arrive
 
-            byte header = Serial.read();
-            byte data1 = Serial.read();
-            byte data2 = Serial.read();
+        byte header = Serial.read();
+        byte data1 = Serial.read();
+        byte data2 = Serial.read();
 
-            if (header == NOTE_ON) {
-                this->omniNoteOn();
-            } 
-            else if (header == NOTE_OFF) {
-                this->omniNoteOff();
-            }
+        last_received_message[0] = header;
+        last_received_message[1] = data1;
+        last_received_message[2] = data2;
 
-            last_received_packet[0] = header;
-            last_received_packet[1] = data1;
-            last_received_packet[2] = data2;
-
-        } else {
-            Serial.println("Invalid packet size. Flushing...");
-            while (Serial.available()) {
-                Serial.read();
-            }
+        if (header == NOTE_ON) {
+            this->omniNoteOn(last_received_message);
+        } 
+        else if (header == NOTE_OFF) {
+            this->omniNoteOff(last_received_message);
         }
+
+        Serial.println(header);
+        
     }
 }
 
-void OmniMIDI::omniNoteOn() {
-    digitalWrite(LED_BUILTIN, HIGH);
+void OmniMIDI::omniNoteOn(midi_message msg) {
+    Serial.print("Note on detected. Header: ");
+    this->notes_on.push_back(msg);
 }
 
-void OmniMIDI::omniNoteOff() {
-    digitalWrite(LED_BUILTIN, LOW);
+void OmniMIDI::omniNoteOff(midi_message msg) {
+    Serial.println("Note off detected.");
 }
